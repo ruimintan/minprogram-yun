0b6daee7b63f03798ab1a85a0b7fb9b051fbc33c
ruimin.tan <ruimin.tan@zhangmen.com>
2021-01-11 15:50:47 +0800

    feat: 修改每日一文api调用

--- pages/articals/index.js
+++ pages/articals/index.js
@@ -5,7 +5,8 @@ Page({
   data: {
     hiddenLoading:true,
     artical:[],//每日一文
-    content:''
+    content:'',
+    articalDate:'20110310'
   },
  
   onLoad: function () {
@@ -16,11 +17,29 @@ Page({
         hiddenLoading: !this.data.hiddenLoading
     });
   },
+  getArticalDate: function(){
+    var now = new Date()
+    var year = now.getFullYear(); //得到年份
+    var month = now.getMonth();//得到月份
+    var date = now.getDate();//得到日期
+    month = month + 1
+    if (month < 10) month = "0" + month
+    if (date < 10) date = "0" + date
+    year=year-9 // 每日一文年限-9获取
+    var articalDate = year + month + date
+    this.setData({
+      articalDate: articalDate
+    });
+  },
   getArtical:function(){ // 获取每日一文
     this.changeHidden()
     var that = this
+    that.getArticalDate()
     wx.request({
-      url:"https://interface.meiriyiwen.com/article/today?dev=1",
+      // url:"https://interface.meiriyiwen.com/article/today?dev=1",
+      // url：https://interface.meiriyiwen.com/article/day?dev=1&date= + 日期
+      //  每日一文api支持日期20110306-20200521
+      url:"https://interface.meiriyiwen.com/article/day?dev=1&date=" + that.data.articalDate,
       success:function(res){
         var data=res.data.data
         console.log(data)
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
     dataList:[],//ONE近七日数据
     sentence:[],//每日一句
     artical:[],//每日一文
+    articalDate:'20110310'
   },
   //事件处理函数
   openArtical: function () {
@@ -59,10 +60,28 @@ Page({
       }
     })
   },
+  getArticalDate: function(){
+    var now = new Date()
+    var year = now.getFullYear(); //得到年份
+    var month = now.getMonth();//得到月份
+    var date = now.getDate();//得到日期
+    month = month + 1
+    if (month < 10) month = "0" + month
+    if (date < 10) date = "0" + date
+    year=year-9 // 每日一文年限-9获取
+    var articalDate = year + month + date
+    this.setData({
+      articalDate: articalDate
+    });
+  },
   getArtical:function(){ // 获取每日一文
     var that = this
+    that.getArticalDate()
     wx.request({
-      url:"https://interface.meiriyiwen.com/article/today?dev=1",
+      // url:"https://interface.meiriyiwen.com/article/today?dev=1",
+      // url：https://interface.meiriyiwen.com/article/day?dev=1&date= + 日期
+      //  每日一文api支持日期20110306-20200521
+      url:"https://interface.meiriyiwen.com/article/day?dev=1&date="+ that.data.articalDate,
       success:function(res){
         var data=res.data.data
         console.log(data)
